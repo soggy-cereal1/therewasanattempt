b49c0f45ebc1f887dafc9138054016da499fa072
soggy-cereal1 <114190885+soggy-cereal1@users.noreply.github.com>
2022-12-22 14:14:55 -0500

    no clue what i did but it keeps wanting documents

---
+++ main.cpp
@@ -0,0 +1,11 @@
+// does this worK? maybe
+// do i know what other setup i might need? no
+// according to vscode site, there's already a C++ compiler in macOS
+// edit the book was copyright 2015
+
+
+#include <iostream> 
+
+int main() {
+    std::cout << "Hello World!";
+}
--- test.cpp
+++
@@ -1,2 +0,0 @@
-// does this worK? maybe
-// do i know what other setup i might need? no
