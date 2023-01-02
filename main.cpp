acf7559b30b23f07b50b9f88026f2d817fa42ead
soggy-cereal1 <114190885+soggy-cereal1@users.noreply.github.com>
2023-01-02 15:12:17 -0500

    things happened
    re-broke things to get rid of the littel blue dot

--- main.cpp
+++ main.cpp
@@ -1,11 +1,36 @@
-// does this worK? maybe
-// do i know what other setup i might need? no
-// according to vscode site, there's already a C++ compiler in macOS
-// edit the book was copyright 2015
+// my first program in C++
+#include <iostream>
+using namespace std;
 
+int pow(int base, int power) {
+  for (int i = 1; i>power; i++) {
+    base*=base;
+  }
+  return base;
+}
 
-#include <iostream> 
-
-int main() {
-    std::cout << "Hello World!";
+int main()
+{
+    int a; 
+    int b;
+    int c;
+    
+    cin >> a;
+    cin >> b;
+    cin >> c;
+    
+    int aSquared = pow(a,2);
+    int bSquared = pow(b,2);
+    
+    int cSquared = pow(c,2);
+  cout << "Hello World! " << endl;
+  cout << "I am a c++ program. " << endl;
+  
+  
+  if (aSquared + bSquared == cSquared) {
+    cout << a << ", " << b << ", and " << c << " form a right triangle." << endl;
+    cout << "a squared is " << aSquared << ", b squared is " << bSquared << ", and their sum is " << aSquared + bSquared << ", which is the same as c squared.";
+    } else {
+    cout << "a and b do not form a right triangle with hypotenuse c.";
+    }
 }
