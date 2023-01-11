699aff47fbb369a49fc779312f9cf5b950482081
soggy-cereal1 <114190885+soggy-cereal1@users.noreply.github.com>
2023-01-10 23:00:39 -0500

    figured out how to make it see blue yay

--- main.py
+++ main.py
@@ -1,114 +1,88 @@
-from depthai_sdk import OakCamera
-import numpy as np
-import cv2
+# TechVidvan Object detection of similar color
 
-webcam = cv2.VideoCapture(0)
-  
-# Start a while loop
-while(1):
-      
-    # Reading the video from the
-    # webcam in image frames
-    _, imageFrame = webcam.read()
-  
-    # Convert the imageFrame in 
-    # BGR(RGB color space) to 
-    # HSV(hue-saturation-value)
-    # color space
-    hsvFrame = cv2.cvtColor(imageFrame, cv2.COLOR_BGR2HSV)
-  
-    # Set range for red color and 
-    # define mask
-    red_lower = np.array([136, 87, 111], np.uint8)
-    red_upper = np.array([180, 255, 255], np.uint8)
-    red_mask = cv2.inRange(hsvFrame, red_lower, red_upper)
-  
-    # Set range for green color and 
-    # define mask
-    green_lower = np.array([25, 52, 72], np.uint8)
-    green_upper = np.array([102, 255, 255], np.uint8)
-    green_mask = cv2.inRange(hsvFrame, green_lower, green_upper)
-  
-    # Set range for blue color and
-    # define mask
-    blue_lower = np.array([94, 80, 2], np.uint8)
-    blue_upper = np.array([120, 255, 255], np.uint8)
-    blue_mask = cv2.inRange(hsvFrame, blue_lower, blue_upper)
-      
-    # Morphological Transform, Dilation
-    # for each color and bitwise_and operator
-    # between imageFrame and mask determines
-    # to detect only that particular color
-    kernal = np.ones((5, 5), "uint8")
-      
-    # For red color
-    red_mask = cv2.dilate(red_mask, kernal)
-    res_red = cv2.bitwise_and(imageFrame, imageFrame, 
-                              mask = red_mask)
-      
-    # For green color
-    green_mask = cv2.dilate(green_mask, kernal)
-    res_green = cv2.bitwise_and(imageFrame, imageFrame,
-                                mask = green_mask)
-      
-    # For blue color
-    blue_mask = cv2.dilate(blue_mask, kernal)
-    res_blue = cv2.bitwise_and(imageFrame, imageFrame,
-                               mask = blue_mask)
-   
-    # Creating contour to track red color
-    contours, hierarchy = cv2.findContours(red_mask,
-                                           cv2.RETR_TREE,
-                                           cv2.CHAIN_APPROX_SIMPLE)
-      
-    for pic, contour in enumerate(contours):
-        area = cv2.contourArea(contour)
-        if(area > 300):
-            x, y, w, h = cv2.boundingRect(contour)
-            imageFrame = cv2.rectangle(imageFrame, (x, y), 
-                                       (x + w, y + h), 
-                                       (0, 0, 255), 2)
-              
-            cv2.putText(imageFrame, "Red Colour", (x, y),
-                        cv2.FONT_HERSHEY_SIMPLEX, 1.0,
-                        (0, 0, 255))    
-  
-    # Creating contour to track green color
-    contours, hierarchy = cv2.findContours(green_mask,
-                                           cv2.RETR_TREE,
-                                           cv2.CHAIN_APPROX_SIMPLE)
-      
-    for pic, contour in enumerate(contours):
-        area = cv2.contourArea(contour)
-        if(area > 300):
-            x, y, w, h = cv2.boundingRect(contour)
-            imageFrame = cv2.rectangle(imageFrame, (x, y), 
-                                       (x + w, y + h),
-                                       (0, 255, 0), 2)
-              
-            cv2.putText(imageFrame, "Green Colour", (x, y),
-                        cv2.FONT_HERSHEY_SIMPLEX, 
-                        1.0, (0, 255, 0))
-  
-    # Creating contour to track blue color
-    contours, hierarchy = cv2.findContours(blue_mask,
-                                           cv2.RETR_TREE,
-                                           cv2.CHAIN_APPROX_SIMPLE)
-    for pic, contour in enumerate(contours):
-        area = cv2.contourArea(contour)
-        if(area > 300):
-            x, y, w, h = cv2.boundingRect(contour)
-            imageFrame = cv2.rectangle(imageFrame, (x, y),
-                                       (x + w, y + h),
-                                       (255, 0, 0), 2)
-              
-            cv2.putText(imageFrame, "Blue Colour", (x, y),
-                        cv2.FONT_HERSHEY_SIMPLEX,
-                        1.0, (255, 0, 0))
-              
-    # Program Termination
-    cv2.imshow("Multiple Color Detection in Real-TIme", imageFrame)
-    if cv2.waitKey(10) & 0xFF == ord('q'):
-        cap.release()
-        cv2.destroyAllWindows()
-        break
+import cv2
+import numpy as np
+# from depthai_sdk import OakCamera
+# import depthai as dai
+
+# # Create pipeline
+# pipeline = dai.Pipeline()
+# cam = pipeline.create(dai.node.ColorCamera)
+# cam.setColorOrder(dai.ColorCameraProperties.ColorOrder.RGB)
+# cam.setPreviewSize(1280,720)
+# xout = pipeline.create(dai.node.XLinkOut)
+# xout.setStreamName("rgb")
+# cam.preview.link(xout.input)
+
+# # Connect to device and start pipeline
+# with dai.Device(pipeline) as device, pyvirtualcam.Camera(width=1280, height=720, fps=20) as uvc:
+#     qRgb = device.getOutputQueue(name="rgb", maxSize=4, blocking=False)
+#     print("UVC running")
+#     while True:
+#         frame = qRgb.get().getFrame()
+#         uvc.send(frame)
+        
+# Reading the image
+img = cv2.imread("IMG_4206.jpg")
+
+# convert to hsv colorspace
+
+# hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+# lower bound and upper bound for blue color
+lower_bound = np.array([64, 10, 10])
+upper_bound = np.array([255, 75, 75])	 
+
+# I can't tell what i did but hte above detects the cube's lines and the below detects light colors
+# I suck at visualizing things what does this mean
+# HSV doesn't work, i tried BGR for ^ and RGB for below the same values but flipped
+
+# HSV is an attempt at the below because what is going on
+
+# lower_bound = np.array([170, 10, 100])
+# upper_bound = np.array([258, 100, 100])	 
+
+
+# find the colors within the boundaries
+mask = cv2.inRange(img, lower_bound, upper_bound)
+
+
+# define kernel size  
+kernel = np.ones((7,7),np.uint8)
+
+# Remove unnecessary noise from mask
+
+mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
+mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
+
+# Segment only the detected region
+segmented_img = cv2.bitwise_and(img, img, mask=mask)
+
+# Find contours from the mask
+
+contours, hierarchy = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+output = cv2.drawContours(segmented_img, contours, -1, (0, 255, 0), 3)
+
+# Showing the output
+
+# cv2.imshow("Output", output)
+
+# lower bound and upper bound for red color
+
+# lower_bound = np.array([5, 22, 22])
+# upper_bound = np.array([340, 25, 24])	 
+
+# Draw contour on original image
+
+output = cv2.drawContours(img, contours, -1, (0, 0, 255), 3)
+
+
+# Showing the output
+
+cv2.imshow("Image", img)
+cv2.imshow("mask", mask)
+
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
