4102f4e80e8e0b2f2b5fb4adc9cca63bc9a445d1
soggy-cereal1 <114190885+soggy-cereal1@users.noreply.github.com>
2023-01-11 09:31:27 -0500

    it can see cyan now

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 import cv2
 import numpy as np
+
 # from depthai_sdk import OakCamera
 # import depthai as dai
 
@@ -22,6 +23,57 @@ import numpy as np
 #         frame = qRgb.get().getFrame()
 #         uvc.send(frame)
         
+
+cap = cv2.VideoCapture(1)
+while True:
+    ret, frame = cap.read()
+    # find the colors within the boundaries
+
+    # lower bound and upper bound for cyan
+
+    lower_bound = np.array([100,100,20])
+    upper_bound = np.array([255,255,90])
+
+    mask = cv2.inRange(frame, lower_bound, upper_bound)
+
+
+    # define kernel size  
+    kernel = np.ones((7,7),np.uint8)
+
+    # Remove unnecessary noise from mask
+
+    mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
+    mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
+
+    # Segment only the detected f
+    segmented_img = cv2.bitwise_and(frame, frame, mask=mask)
+
+    # Find contours from the mask
+
+    contours, hierarchy = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    output = cv2.drawContours(segmented_img, contours, -1, (0, 255, 0), 3)
+
+    # Showing the output
+
+    # cv2.imshow("Output", output) 
+
+    # Draw contour on original image
+
+    output = cv2.drawContours(frame, contours, -1, (0, 0, 255), 3)
+
+
+    # Showing the output
+
+    cv2.imshow("cam", frame)
+    cv2.imshow("mask", mask)
+    if (cv2.waitKey(30) == 27):
+       break
+
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
 # Reading the image
 img = cv2.imread("bettersleeve2.jpg")
 
@@ -87,7 +139,3 @@ output = cv2.drawContours(img, contours, -1, (0, 0, 255), 3)
 
 cv2.imshow("Image", img)
 cv2.imshow("mask", mask)
-
-
-cv2.waitKey(0)
-cv2.destroyAllWindows()
