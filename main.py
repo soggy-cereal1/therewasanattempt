6c8024a6aa31e648bd25fca374b4a85b55bc6c08
soggy-cereal1 <114190885+soggy-cereal1@users.noreply.github.com>
2023-01-14 11:41:25 -0500

    I have no clue

--- main.py
+++ main.py
@@ -2,11 +2,12 @@
 
 import cv2
 import numpy as np
+import time 
 
-# from depthai_sdk import OakCamera
-# import depthai as dai
+from depthai_sdk import OakCamera
+import depthai as dai
 
-# # Create pipeline
+# Create pipeline
 # pipeline = dai.Pipeline()
 # cam = pipeline.create(dai.node.ColorCamera)
 # cam.setColorOrder(dai.ColorCameraProperties.ColorOrder.RGB)
@@ -31,10 +32,29 @@ while True:
 
     # lower bound and upper bound for cyan
 
-    lower_bound = np.array([100,100,20])
-    upper_bound = np.array([255,255,90])
+    lower_bound_c = np.array([100,100,90])
+    upper_bound_c = np.array([255,255,130])
 
-    mask = cv2.inRange(frame, lower_bound, upper_bound)
+    # lower and upper bound for magenta
+
+    lower_bound_m = np.array([100, 20, 100])
+    upper_bound_m = np.array([255,80,255])
+
+    # lower and upper bound for yellow
+
+    lower_bound_y = np.array([70, 130, 130])
+    upper_bound_y = np.array([120, 255, 255])
+
+    #red
+    lower_bound_r = np.array([10, 10, 130])
+    upper_bound_r = np.array([70, 50, 255])
+
+    #blue # WIP
+    lower_bound_b = np.array([100, 10, 10])
+    upper_bound_b = np.array([255, 80, 80])
+
+
+    mask = cv2.inRange(frame, lower_bound_y, upper_bound_y)
 
 
     # define kernel size  
@@ -47,95 +67,21 @@ while True:
 
     # Segment only the detected f
     segmented_img = cv2.bitwise_and(frame, frame, mask=mask)
-
     # Find contours from the mask
 
     contours, hierarchy = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     output = cv2.drawContours(segmented_img, contours, -1, (0, 255, 0), 3)
 
-    # Showing the output
-
-    # cv2.imshow("Output", output) 
-
     # Draw contour on original image
 
-    output = cv2.drawContours(frame, contours, -1, (0, 0, 255), 3)
+    output = cv2.drawContours(frame, contours, -1, (0, 255, 0), 3)
 
+    amount_c = contours
 
     # Showing the output
 
-    cv2.imshow("cam", frame)
+    cv2.imshow("cam", output)
     cv2.imshow("mask", mask)
     if (cv2.waitKey(30) == 27):
        break
-
-
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-
-# Reading the image
-img = cv2.imread("bettersleeve2.jpg")
-
-# convert to hsv colorspace
-
-# hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-
-# lower bound and upper bound for blue color
-# lower_bound = np.array([64, 10, 10])
-# upper_bound = np.array([255, 60, 60])	 
-
-# lower bound and upper bound for red color
-
-# lower_bound = np.array([20, 20, 60])
-# upper_bound = np.array([70, 70, 255])	
-
-# lower bound and upper bound for cyan
-
-# lower_bound = np.array([100,100,20])
-# upper_bound = np.array([255,255,90])
-
-# lower and upper bound for magenta
-
-# lower_bound = np.array([60, 10, 60])
-# upper_bound = np.array([255,50,255])
-
-# lower and upper bound for yellow
-
-# lower_bound = np.array([20, 100, 100])
-# upper_bound = np.array([40, 255, 255])
-
-# find the colors within the boundaries
-mask = cv2.inRange(img, lower_bound, upper_bound)
-
-
-# define kernel size  
-kernel = np.ones((7,7),np.uint8)
-
-# Remove unnecessary noise from mask
-
-mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
-mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
-
-# Segment only the detected region
-segmented_img = cv2.bitwise_and(img, img, mask=mask)
-
-# Find contours from the mask
-
-contours, hierarchy = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-output = cv2.drawContours(segmented_img, contours, -1, (0, 255, 0), 3)
-
-# Showing the output
-
-# cv2.imshow("Output", output) 
-
-# Draw contour on original image
-
-output = cv2.drawContours(img, contours, -1, (0, 0, 255), 3)
-
-
-# Showing the output
-
-cv2.imshow("Image", img)
-cv2.imshow("mask", mask)
