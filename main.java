d63830044925b598c1fc4aebf890b6b0b9819848
soggy-cereal1 <114190885+soggy-cereal1@users.noreply.github.com>
2023-01-15 19:43:06 -0500

    Update main.java

--- main.java
+++ main.java
@@ -1,17 +1,42 @@
+package opencv;
 import org.opencv.videoio.*;
 import org.opencv.core.*;
 import java.lang.Object;
 import java.util.Arrays;
+import org.opencv.core.*;
 
-public class main {
+public class main 
+{
 
-    public float cap = VideoCapture;
+    float cap = Core.VideoCapture;
+
+    int[][] lower_bounds = {{100, 100, 90}, {100, 20, 100}, {70, 130, 130}};
+    int[][] upper_bounds = {{255, 255, 130}, {255, 80, 255}, {120, 255, 255}};
+
     while (true) {
-        ret, frame = cap.read();
-        int[] lower_bound_c = new int[3];
-        int[] upper_bound_c = new int[3];
+    
+        frame = VideoCapture.read(cap);
 
-        mask = cv2.inRange(frame, lower_bound_c, upper_bound_c)
+
+        for(int i=0; i<4; i++) {
+            Mat mask = cv2.inRange(frame, lower_bounds[i], upper_bounds[i]);
+            
+            kernel = np.ones((7,7),np.uint8);
+
+            mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel);
+            mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel);
+            
+            segmented_img = cv2.bitwise_and(frame, frame, mask=mask);
+
+            contours, hierarchy = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE);
+
+            output = cv2.drawContours(segmented_img, contours, -1, (0, 255, 0), 3);
+
+            # Draw contour on original image
+
+            output = cv2.drawContours(frame, contours, -1, (0, 255, 0), 3);
+        }
+
     }
 
 }
