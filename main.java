1283555e7a9c260e48864f9a60765649c69f50ef
soggy-cereal1 <114190885+soggy-cereal1@users.noreply.github.com>
2023-01-17 09:32:03 -0500

    I barely know anymore, time lost obsessing this

--- main.java
+++ main.java
@@ -1,47 +1,55 @@
-package opencv;
-import org.opencv.videoio.*;
-import org.opencv.core.*;
-import java.lang.Object;
-import java.util.Arrays;
-import org.opencv.core.*;
-import org.openftc.easyopencv.OpenCvCamera;
+package org.firstinspires.ftc.teamcode.subsystems;
 
-public class main 
-{
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.MatOfPoint2f;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+import org.openftc.easyopencv.OpenCvPipeline;
 
-    OpenCvCamera = camera;
+import java.util.List;
+import java.util.ArrayList;
 
-    camera.openCameraDevice();
+public class BarCodeDetector extends OpenCvPipeline {
 
-    new Scalar[] {(100, 100, 90), (100, 20, 100), (70, 130, 130)};
+    Mat frame = new Mat();
 
-    int[][] lower_bounds = {{100, 100, 90}, {100, 20, 100}, {70, 130, 130}};
-    int[][] upper_bounds = {{255, 255, 130}, {255, 80, 255}, {120, 255, 255}};
+    public Mat Masking(Mat input) {
 
-    while (true) {
-    
-        frame = VideoCapture.read(cap);
+        int[][] lower_bounds = {{100, 100, 90}, {100, 20, 100}, {70, 130, 130}};
+        int[][] upper_bounds = {{255, 255, 130}, {255, 80, 255}, {120, 255, 255}};
+
+        while (true) {
+        
+            // frame = VideoCapture.read(cap);
 
 
-        for(int i=0; i<4; i++) {
-            Mat mask = cv2.inRange(frame, lower_bounds[i], upper_bounds[i]);
-            
-            // kernel = np.ones((7,7),np.uint8);
+            for(int i=0; i<4; i++) {
+                
+                Mat mask = Core.inRange(frame, lower_bounds[i], upper_bounds[i]);
+                
+                // kernel = np.ones((7,7),np.uint8);
 
-            mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel);
-            mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel);
-            
-            // segmented_img = cv2.bitwise_and(frame, frame, mask=mask);
+                mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel);
+                mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel);
 
-            // contours, hierarchy = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE);
+                if(Mat) {
+                    return i;
+                } 
+                
+                // segmented_img = cv2.bitwise_and(frame, frame, mask=mask);
 
-            // output = cv2.drawContours(segmented_img, contours, -1, (0, 255, 0), 3);
+                // contours, hierarchy = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE);
 
-            // # Draw contour on original image
+                // output = cv2.drawContours(segmented_img, contours, -1, (0, 255, 0), 3);
 
-            // output = cv2.drawContours(frame, contours, -1, (0, 255, 0), 3);
+                // # Draw contour on original image
+
+                // output = cv2.drawContours(frame, contours, -1, (0, 255, 0), 3);
+            }
+
         }
-
     }
-
 }
