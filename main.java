cf4f62eb6b53f5a746d699f17b9378c629048eca
soggy-cereal1 <114190885+soggy-cereal1@users.noreply.github.com>
2023-01-18 07:33:01 -0500

    added something and copy pasted more code over it might work?

--- main.java
+++ main.java
@@ -16,68 +16,87 @@ public static void main(String[] args) {
 
     Mat frame = new Mat();
     frame = VideoCapture(1);
-    private ElementLocation location = ElementLocation.NONE;
+    private IndicatedLocation location = IndicatedLocation.NONE;
 
-    public class Mat Masking(Mat input) {
+    public class Mat processFrame(Mat input){
 
-        List<Scalar> lower_bounds = new ArrayList<>();
-        lower_bounds.addAll((100, 100, 90), (100, 20, 100), (70, 130, 130));
+        if(mat.empty()){
+            location=IndicatedLocation.NONE;
+            return input;
+        }
 
-        List<Scalar> upper_bounds = new ArrayList<>();
-        upper_bounds.addAll((255, 255, 130), (255, 80, 255), (120, 255, 255));
-        Mat thresh = new Mat();
+        List<Scalar> lower_bounds=new ArrayList<>();
+        lower_bounds.addAll((100,100,90),(100,20,100),(70,130,130));
 
-        while (true) {
-        
-            frame = VideoCapture.read(cap);
+        List<Scalar> upper_bounds=new ArrayList<>();
+        upper_bounds.addAll((255,255,130),(255,80,255),(120,255,255));
+        Mat thresh=new Mat();
+
+        while(true){
+
+            frame=VideoCapture.read(cap);
 
 
-            for(int i=0; i<4; i++) {
+            for(int i=0;i<4;i++){
 
+                Core.inRange(frame,lower_bounds[i],upper_bounds[i],thresh);
 
-                // the above is supposed to do what the below is supposed to do
-
-                Mat mask = new Mat();
-                Imgproc.Canny(thresh, mask, 100, 300);
+                Mat mask=new Mat();
+                Imgproc.Canny(thresh,mask,100,300);
                 thresh.release();
                 // oftentimes the edges are disconnected, findContours connects these edges
                 // then find the bounding rectangles of those contours
-                
-                List<MatOfPoint> contours = new ArrayList<>();
-                Mat hierarchy = new Mat();
-                Imgproc.findContours(edges, contours, hierarchy, Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE);
+
+                List<MatOfPoint> contours=new ArrayList<>();
+                Mat hierarchy=new Mat();
+                Imgproc.findContours(edges,contours,hierarchy,Imgproc.RETR_TREE,Imgproc.CHAIN_APPROX_SIMPLE);
                 hierarchy.release();
                 edges.release();
                 MatOfPoint2f contoursPoly;
-                Rect[] boundRect = new Rect[contours.size()];
+                Rect[]boundRect=new Rect[contours.size()];
+
+                for(int j=0;j<contours.size();j++){
+                    contoursPoly=new MatOfPoint2f();
+                    Imgproc.approxPolyDP(new MatOfPoint2f(contours.get(i).toArray()),contoursPoly,3,true);
+                    contours.get(j).release();
+                    boundRect[j]=Imgproc.boundingRect(new MatOfPoint(contoursPoly.toArray()));
+                    contoursPoly.release();
+                }
 
 
-                if(Rect boundRect.length = 240) {
-                    location = ElementLocation(i);
+                if(Rect boundRect[i].x>200){
+                    location=IndicatedLocation(i);
                 } else {
                     break;
                 }
-                
+
             }
 
         }
+    }
 
-        public enum ElementLocation {
-            LEFT(1),
-            MIDDLE(2),
-            RIGHT(3),
-            NONE(0);
-    
-            int val;
+    public enum IndicatedLocation {
+        LEFT(1),
+        MIDDLE(2),
+        RIGHT(3),
+        NONE(0);
 
-    
-            ElementLocation(int i) {
-                val = i;
-            }
-    
-            public int val() {
-                return val;
-            }
+        int val;
+
+
+        ElementLocation(int i) {
+            val = i;
         }
+
+        public int val() {
+            return val;
+        }
+    }
+    public ElementLocation getLocation() {
+        return this.location;
+    }
+
+    public int getLocationInt() {
+        return location.val;
     }
 }
